3e3a193ea1b8892d9ae9515cdc4a803afb92525b
Danisaxas <cuentaregali178@gmail.com>
2025-03-02 20:03:20 -0400

    holas

--- bot.php
+++ bot.php
@@ -1,7 +1,5 @@
 <?php
 $config = require 'config.php';
-$token = $config['TOKEN'];
-$api_url = $config['API_URL'];
 
 $update = json_decode(file_get_contents('php://input'), true);
 if (!$update) {
@@ -12,17 +10,21 @@ $chat_id = $update['message']['chat']['id'] ?? null;
 $text = $update['message']['text'] ?? '';
 
 if ($text) {
-    if ($text == '/start') {
-        require 'index/user/start.php';
-    } elseif ($text == '/help') {
-        require 'index/user/help.php';
-    } elseif ($text == '/ban') {
-        require 'index/admin/ban.php';
+    $command = explode(' ', $text)[0]; // Extrae el comando sin argumentos
+
+    $commands = [
+        '/start' => 'index/user/start.php',
+        '/help' => 'index/user/help.php',
+        '/ban' => 'index/admin/ban.php'
+    ];
+
+    if (array_key_exists($command, $commands)) {
+        require $commands[$command];
     }
 }
 
 function sendMessage($chat_id, $message) {
-    global $api_url;
-    file_get_contents($api_url . "sendMessage?chat_id=$chat_id&text=" . urlencode($message));
+    global $config;
+    file_get_contents($config['API_URL'] . "sendMessage?chat_id=$chat_id&text=" . urlencode($message));
 }
 ?>
