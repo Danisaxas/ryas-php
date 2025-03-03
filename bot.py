01e6445bd1be3b9f3cbbd6ad96dedab0e9d5baff
Danisaxas <cuentaregali178@gmail.com>
2025-03-02 20:41:21 -0400

    php old new python

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 from configs._def_main_ import *
+from pyrogram import Client
 
-app = Client("Ryas",
-        api_id = conta('apid'),
-            api_hash = conta('hasd'),
-                bot_token = conta('token'),
-                    plugins=dict(root="pluss"))
+# Crear instancia del bot de Pyrogram
+app = Client(
+    "Ryas",
+    api_id=conta('apid'),
+    api_hash=conta('hasd'),
+    bot_token=conta('token'),
+    plugins=dict(root="pluss")
+)
 
+# Respuesta al callback query
 @app.on_callback_query()
 async def callpri(app, callback_query):
     if callback_query.message.reply_to_message is not None and callback_query.message.reply_to_message.from_user.id != callback_query.from_user.id:
@@ -14,7 +19,8 @@ async def callpri(app, callback_query):
     else:
         await callback_query.continue_propagation()
 
+# Iniciar el bot
 if app:
-    cls(),app.run()
+    app.run()  # Ejecuta el bot de manera adecuada
 else:
-    print('Error faltas de argumentos.')
+    print('Error faltan argumentos.')
